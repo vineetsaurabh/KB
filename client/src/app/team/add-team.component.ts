76a40b15576c5ec0e24c0298d2749dca85efa706
Vineet Saurabh <vineetsaurabh@gmail.com>
2018-09-02 06:05:47 +0530

    SPOC Autocomplete selection for Team

--- client/src/app/team/add-team.component.ts
+++ client/src/app/team/add-team.component.ts
@@ -1,25 +1,29 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { NgForm } from '@angular/forms';
+import { NgForm, FormBuilder } from '@angular/forms';
 import { MatDialogRef } from '@angular/material';
 import { ToastrService } from 'ngx-toastr';
 
 import { Team } from './team.model';
 import { TeamService } from './team.service';
+import { User } from '../user/user.model';
+import { UserService } from '../user/user.service';
+import { SelectUserComponent } from '../common/select-user-component';
 
 @Component({
     templateUrl: './add-team.component.html'
 })
-export class AddTeamComponent {
+export class AddTeamComponent extends SelectUserComponent {
 
     team: Team = new Team();
 
     constructor(
-        private router: Router,
+        protected fb: FormBuilder,
+        protected userService: UserService,
         private teamService: TeamService,
         private toastService: ToastrService,
         public dialogRef: MatDialogRef<AddTeamComponent>) {
-
+        super(fb, userService);
     }
 
     createTeam(userForm: NgForm): void {
@@ -28,6 +32,14 @@ export class AddTeamComponent {
                 this.toastService.success(`User ${this.team.teamName} added`);
                 this.dialogRef.close(false);
             });
-    };
+    }
+
+    ngOnInit() {
+        super.ngOnInit();
+    }
+
+    setSpoc(user: User) {
+        this.team.spoc = user;
+    }
 
 }
--- client/src/app/team/edit-team.component.ts
+++ client/src/app/team/edit-team.component.ts
@@ -1,17 +1,19 @@
-import { Component, OnInit, Injectable, Inject } from '@angular/core';
-import { NgForm, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, Injectable, Inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { ToastrService } from 'ngx-toastr';
 
 import { Team } from './team.model';
 import { TeamService } from './team.service';
 import { User } from '../user/user.model';
+import { UserService } from '../user/user.service';
+import { SelectUserComponent } from '../common/select-user-component';
 
 @Injectable()
 @Component({
     templateUrl: './edit-team.component.html'
 })
-export class EditTeamComponent implements OnInit {
+export class EditTeamComponent extends SelectUserComponent {
 
     public team: Team = {
         teamid: '',
@@ -25,28 +27,36 @@ export class EditTeamComponent implements OnInit {
     teamForm: FormGroup;
 
     constructor(
+        protected fb: FormBuilder,
+        protected userService: UserService,
         private teamService: TeamService,
-        private fb: FormBuilder,
         private toastService: ToastrService,
         public dialogRef: MatDialogRef<EditTeamComponent>,
         @Inject(MAT_DIALOG_DATA) public data: Team) {
+        super(fb, userService);
         this.team = this.data;
     }
 
     ngOnInit() {
+        super.ngOnInit();
+        this.userNameForm.get('userNameGroup').setValue(this.team.spoc);
         this.teamForm = this.fb.group({
             teamid: 0,
             name: ['', [Validators.required]],
             description: ['', [Validators.required]],
-        })
+        });
     }
 
-    updateTeam(userForm: NgForm) {
+    updateTeam() {
         this.teamService.updateTeam(this.team)
             .subscribe(res => {
                 this.toastService.success(`User ${this.team.teamName} updated`);
                 this.dialogRef.close(false);
             });
+    }
+
+    setSpoc(user: User) {
+        this.team.spoc = user;
     }
 
 }
